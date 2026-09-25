bfb7e4a90056dea87711916da2af93cce4934461
syoch <syoch64@gmail.com>
2026-09-25 15:15:46 +0900

    update: bumped nix inputs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-26.05";
 
   inputs.roboenv.url = "github:nnctroboticsclub/roboenv-nix";
   inputs.roboenv.inputs.nixpkgs.follows = "nixpkgs";
